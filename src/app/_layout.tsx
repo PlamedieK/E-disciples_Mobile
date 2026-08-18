b06f5f154ed16be78d42943068db890a6e2207d5
PlamedieK <pkimpambudi@gmail.com>
2026-08-18 14:33:00 +0200

    add plugins nativewind(tailwindcss)

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,18 +1,8 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useColorScheme } from 'react-native';
+import { Stack } from "expo-router";
+//import "./global.css"; // Ajustez le chemin selon l'emplacement du fichier
+import "../../global.css"; // Les deux "../" permettent de remonter de deux dossiers (sortir de app, puis de src)
 
-import { AnimatedSplashOverlay } from '@/components/animated-icon';
-import AppTabs from '@/components/app-tabs';
 
-SplashScreen.preventAutoHideAsync();
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <AnimatedSplashOverlay />
-      <AppTabs />
-    </ThemeProvider>
-  );
+export default function RootLayout() {
+  return <Stack />;
 }
