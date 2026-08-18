c7bd500ad9376938ec5b22bc991ff606fcdf5812
PlamedieK <pkimpambudi@gmail.com>
2026-08-18 14:44:28 +0200

    feat: don't finish task

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,8 +1,8 @@
 import { Stack } from "expo-router";
 //import "./global.css"; // Ajustez le chemin selon l'emplacement du fichier
-import "../../global.css"; // Les deux "../" permettent de remonter de deux dossiers (sortir de app, puis de src)
+import "../../global.css"; // Les deux "../" permettent de remonter de deux dossiers (sortir de app, puis de src)screenOptions={{ headerShown: false }}
 
 
 export default function RootLayout() {
-  return <Stack />;
+  return <Stack  />;
 }
